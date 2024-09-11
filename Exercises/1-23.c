331066e549ec7ed9dfc52ff88dfcc665c21efaef
Ian Fuller <ianfullerb@gmail.com>
2024-09-11 13:54:26 -0400

    Exercise 1-23 is in progress.

---
+++ Exercises/1-23.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define SIZE 2000
+
+/* This program contains extra comments
+inside it to test its own functionality */
+
+int main() {
+	// variable declaration
+	char input[SIZE];
+	char output[SIZE];
+	int /* ignore this */ c;
+	int length = 0;
+
+	// get user input
+	while ((c = getchar()) != EOF) {
+		input[length] = c;
+		++length;
+	}
+
+	// scan through the input to skip over comments
+	int i;
+	int j = 0;
+	for (i = 0; i < length; ++i) {
+		if (input[i] == '/') { // the beginning of a comment has been located
+			if (input[i + 1] == '/') { // the comment is single-line
+				while(input[i] != '\n') {
+					++i;
+				}
+				++i;
+			}
+			else if (input[i + 1] == '*') { // the comment is multi-line
+				while(input[i + 1] != '\n' && input[i + 1] != '/') { // while the comment has not ended
+					++i;
+				}
+				++i;
+				++i;
+				if (input[i] == '\n') { // exclude the newline character if one has been found
+					++i;
+				}
+			}
+		}
+
+		output[j] = input[i];
+		++j;
+	}
+
+	// output
+	for (i = 0; i < j; ++i) {
+		printf("%c", output[i]);
+	}
+
+	return 0;
+}
